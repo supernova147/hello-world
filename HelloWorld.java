10246d52bceedff115cbdd977d2d558faec33469
Alexander <irizarrya123@gmail.com>
2024-08-26 22:16:52 -0400

    Added new modified text
    added new system print statement.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
    
    System.out.println("Hello world!");
    
-   
+   System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
    
    }
 }
